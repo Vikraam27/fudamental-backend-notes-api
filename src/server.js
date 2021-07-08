52b82cea8bb24713a2634042fefdece0b0284b4c
Vikraam27 <49831979+Vikraam27@users.noreply.github.com>
2021-07-08 18:46:52 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
-    port: 8000,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
